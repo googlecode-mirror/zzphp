277fc34b98c0b5f0e99f50f118ff81312dccf681
alg4net <alg4net@030ae146-9c40-11dd-93a9-677c3c677ea2>
2010-05-27 18:24:31 +0000

    introduced ZZ_ResultSet for results, now everything can be acessed as result['prop'] or result->prop and everything implements Iterator

--- branches/alganet/library/ZZ.php
+++ branches/alganet/library/ZZ.php
@@ -2,6 +2,7 @@
 
 require_once 'ZZ/FuncaoInvalidaException.php';
 require_once 'ZZ/SaidaInvalidaException.php';
+require_once 'ZZ/ResultSet.php';
 
 abstract class ZZ
 {
@@ -22,97 +23,28 @@ abstract class ZZ
     const ALFABETO_PORTUGAL = 'portugal';
     const ALFABETO_NAMES = 'names';
     const ALFABETO_LAPD = 'lapd';
-    const SAIDA_ARRAY = 'Array';
-    const SAIDA_TABULAR = 'Tabular';
-    const SAIDA_TEXTO = 'Texto';
-    const SAIDA_LISTA = 'Lista';
 
-    protected static function transformArray($zzsaida)
-    {
-        return array_filter(explode(PHP_EOL, $zzsaida));
-    }
-
-    protected static function normalizar($nome)
-    {
-        $nome = htmlentities(strtolower(utf8_decode($nome)));
-        $nome = preg_replace('#&(.)(acute|cedil|circ|ring|tilde|uml);#', '$1', $nome);
-        $nome = preg_replace('#^[^A-Za-z0-9]*(.*)#', '$1', $nome);
-        $nome = preg_replace('#(.*?)[^A-Za-z0-9]*$#', '$1', $nome);
-        $nome = preg_replace('#[^A-Za-z0-9]#', '_', $nome);
-        return utf8_encode($nome);
-    }
-
-    protected static function transformLista($zzsaida)
-    {
-        $linhas = self::transformArray($zzsaida);
-        $resultados = array();
-        foreach ($linhas as $linha) {
-            $chaveValor = preg_split('#\s{2,}#', $linha);
-            $chaveValor[0] = self::normalizar($chaveValor[0]);
-            if (2 !== count($chaveValor))
-                continue;
-            if (isset($resultados[$chaveValor[0]])) {
-                if (is_array($resultados[$chaveValor[0]])) {
-                    $resultados[$chaveValor[0]][] = $chaveValor[1];
-                } else {
-                    $resultados[$chaveValor[0]] = array($resultados[$chaveValor[0]], $chaveValor[1]);
-                }
-            } else {
-                $resultados[$chaveValor[0]] = $chaveValor[1];
-            }
-        }
-        return $resultados;
-    }
-
-    protected static function transformTexto($zzsaida)
-    {
-        return trim($zzsaida);
-    }
-
-    protected static function transformTabular($zzsaida)
-    {
-        $linhas = self::transformArray($zzsaida);
-        $cabecalho = array();
-        $resultados = array();
-        foreach ($linhas as $numero => $linha) {
-            if (0 === $numero) {
-                $cabecalho = preg_split('#\s+#', $linha);
-                continue;
-            }
-            $cols = preg_split('#\s{2,}#', $linha);
-            if (count($cols) !== count($cabecalho)) {
-                $cabecalho += array_flip(range(0, count($cols) - 1));
-                $cols += array_flip(range(0, count($cabecalho) - 1));
-            }
-            $cabecalho = array_map(array(__CLASS__, 'normalizar'), $cabecalho);
-            $resultados[self::normalizar(array_shift($cols))] = array_combine(array_slice($cabecalho, 1), $cols);
-        }
-        return $resultados;
-    }
-
-    public static function funcoeszz($funcao, $tratamento, array $argumentos=array())
+    public static function funcoeszz($funcao, $tipoDeResultado, array $argumentos=array())
     {
         if (!ctype_alnum($funcao)) {
             throw new ZZ_FuncaoInvalidaException($funcao, 1);
         }
-        $tratamento = array(__CLASS__, "transform$tratamento");
-        if (!method_exists($tratamento[0], $tratamento[1])) {
-            throw new ZZ_SaidaInvalidaException($tratamento);
+        if (!method_exists('ZZ_ResultSet', $tipoDeResultado)) {
+            throw new ZZ_SaidaInvalidaException($tipoDeResultado);
         }
         $shellArgs = implode(' ', $argumentos);
         $zzsaida = shell_exec("funcoeszz $funcao $shellArgs");
-        $saidaTratada = call_user_func($tratamento, $zzsaida);
         if (false !== strpos($zzsaida, 'Função inexistente')) {
             throw new ZZ_FuncaoInvalidaException($funcao, 2);
         }
-        return $saidaTratada;
+        return new ZZ_ResultSet($zzsaida, $tipoDeResultado);
     }
 
     public static function alfabeto($tipo, $palavra)
     {
         return self::funcoeszz(
             'alfabeto',
-            self::SAIDA_ARRAY,
+            ZZ_ResultSet::ARR,
             array("--$tipo", $palavra)
         );
     }
@@ -121,7 +53,7 @@ abstract class ZZ
     {
         return self::funcoeszz(
             'dolar',
-            self::SAIDA_TABULAR
+            ZZ_ResultSet::TABELA
         );
     }
 
@@ -129,7 +61,7 @@ abstract class ZZ
     {
         return self::funcoeszz(
             'moeda',
-            self::SAIDA_TABULAR
+            ZZ_ResultSet::TABELA
         );
     }
 
@@ -137,7 +69,7 @@ abstract class ZZ
     {
         return self::funcoeszz(
             'whoisbr',
-            self::SAIDA_LISTA,
+            ZZ_ResultSet::LISTA,
             array($alvo)
         );
     }
--- branches/alganet/tests/library/ZZTest.php
+++ branches/alganet/tests/library/ZZTest.php
@@ -12,7 +12,7 @@ class ZZTest extends PHPUnit_Framework_TestCase
      */
     public function testFuncoeszzNomeInvalido()
     {
-        ZZ::funcoeszz('foo#bar%baz', ZZ::SAIDA_ARRAY);
+        ZZ::funcoeszz('foo#bar%baz', ZZ_ResultSet::ARR);
     }
 
     /**
@@ -20,14 +20,14 @@ class ZZTest extends PHPUnit_Framework_TestCase
      */
     public function testFuncoeszzFuncaoInexistente()
     {
-        ZZ::funcoeszz('helloween', ZZ::SAIDA_ARRAY);
+        ZZ::funcoeszz('helloween', ZZ_ResultSet::ARR);
     }
 
     public function testFuncoeszz()
     {
         $this->assertContains(
             'Ajuda das Funções ZZ',
-            ZZ::funcoeszz('ajuda', ZZ::SAIDA_TEXTO)
+            (string) ZZ::funcoeszz('ajuda', ZZ_ResultSet::TEXTO)
         );
     }
 
@@ -42,62 +42,61 @@ class ZZTest extends PHPUnit_Framework_TestCase
     public function testDolar()
     {
         $cotacao = ZZ::dolar();
-        $this->assertArrayHasKey('comercial', $cotacao);
-        $this->assertArrayHasKey('paralelo', $cotacao);
-        $this->assertArrayHasKey('turismo', $cotacao);
+        $this->assertArrayHasKey('comercial', $cotacao->getArrayCopy());
+        $this->assertArrayHasKey('paralelo', $cotacao->getArrayCopy());
+        $this->assertArrayHasKey('turismo', $cotacao->getArrayCopy());
         $this->miniTesteCotacao($cotacao['comercial']);
         $this->miniTesteCotacao($cotacao['paralelo']);
         $this->miniTesteCotacao($cotacao['turismo']);
+        $this->assertEquals($cotacao->comercial, $cotacao['comercial']);
     }
 
     public function miniTesteCotacao($subcotacao)
     {
-        $this->assertArrayHasKey('compra', $subcotacao);
-        $this->assertArrayHasKey('venda', $subcotacao);
-        $this->assertArrayHasKey('hora', $subcotacao);
+        $this->assertArrayHasKey('compra', $subcotacao->getArrayCopy());
+        $this->assertArrayHasKey('venda', $subcotacao->getArrayCopy());
+        $this->assertArrayHasKey('hora', $subcotacao->getArrayCopy());
     }
 
     public function testMoeda()
     {
         $moeda = ZZ::moeda();
-        foreach ($moeda as $submoeda) $this->miniTesteMoeda($submoeda);
+        foreach ($moeda as $submoeda)
+            $this->miniTesteMoeda($submoeda);
     }
 
     public function miniTesteMoeda($submoeda)
     {
-        $this->assertArrayHasKey('compra', $submoeda);
-        $this->assertArrayHasKey('venda', $submoeda);
-        $this->assertArrayHasKey('var', $submoeda);
-        $this->assertArrayHasKey('hora', $submoeda);
-        $this->assertArrayHasKey('moeda', $submoeda);
+        $this->assertArrayHasKey('compra', $submoeda->getArrayCopy());
+        $this->assertArrayHasKey('venda', $submoeda->getArrayCopy());
+        $this->assertArrayHasKey('var', $submoeda->getArrayCopy());
+        $this->assertArrayHasKey('hora', $submoeda->getArrayCopy());
+        $this->assertArrayHasKey('moeda', $submoeda->getArrayCopy());
     }
 
     public function testWhoisbrDominio()
     {
         $whoisbr = ZZ::whoisbr('google.com.br');
-        $this->assertTrue(is_array($whoisbr));
         //amostragem
-        $this->assertArrayHasKey('dominio', $whoisbr);
-        $this->assertArrayHasKey('entidade', $whoisbr);
+        $this->assertArrayHasKey('dominio', $whoisbr->getArrayCopy());
+        $this->assertArrayHasKey('entidade', $whoisbr->getArrayCopy());
     }
 
     public function testWhoisbrID()
     {
         $whoisbr = ZZ::whoisbr('COAGO');
-        $this->assertTrue(is_array($whoisbr));
         //amostragem
-        $this->assertArrayHasKey('id', $whoisbr);
-        $this->assertArrayHasKey('nome', $whoisbr);
-        $this->assertArrayHasKey('criado', $whoisbr);
+        $this->assertArrayHasKey('id', $whoisbr->getArrayCopy());
+        $this->assertArrayHasKey('nome', $whoisbr->getArrayCopy());
+        $this->assertArrayHasKey('criado', $whoisbr->getArrayCopy());
     }
 
     public function testWhoisbrEntidade()
     {
         $whoisbr = ZZ::whoisbr('006.947.284/0001-04');
-        $this->assertTrue(is_array($whoisbr));
         //amostragem
-        $this->assertArrayHasKey('entidade', $whoisbr);
-        $this->assertArrayHasKey('criado', $whoisbr);
+        $this->assertArrayHasKey('entidade', $whoisbr->getArrayCopy());
+        $this->assertArrayHasKey('criado', $whoisbr->getArrayCopy());
     }
 
 }
